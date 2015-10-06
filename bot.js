123b87983b4d666108145f53dfdd6df351a18f22
Christopher B. Ferris <chrisfer@us.ibm.com>
2015-10-05 21:45:16 -0400

    add handler, remove package.son

--- bot.js
+++ bot.js
@@ -1,37 +1,48 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
+var http = require('http');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: 'sekrit' });
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || 7777);
+
 // Start server
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
-    res.end('no such location')
-  })
-}).listen(port, host)
-
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
+    res.end('no such location');
+    console.log("oops?");
+    console.log(req.rawHeaders);
+  });
+}).listen(port, host);
 
 handler.on('*', function (event) {
   console.log('Received an %s event for %s to %s',
     event.payload.action,
     event.payload.repository.name,
-    event.payload.ref)
-})
+    event.payload.ref);
+});
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message);
+});
 
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref)
-})
+    event.payload.ref);
+});
 
 handler.on('issues', function (event) {
   console.log('Received an issue event for %s action=%s: #%d %s',
     event.payload.repository.name,
     event.payload.action,
     event.payload.issue.number,
-    event.payload.issue.title)
-})
+    event.payload.issue.title);
+});
+
+handler.on('issue_comment', function (event) {
+  console.log('Received an issue comment event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title);
+});
